66d75932ded1b043179e696f0faa554b1740c6dc
muthukumar-29 <muthukumarm.2903@gmail.com>
2026-02-19 11:30:10 +0530

    fix SPA problem

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'  // relative to /client, so full path = client/dist ✓
+  }
 })
